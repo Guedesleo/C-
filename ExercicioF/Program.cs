cd83580e1cc8fc0c9129b000b63bdae779e52d55
Guedesleo <leonardo.guedes.rocha@gmail.com>
2019-08-09 09:06:32 -0300

    Exercicio F e G

---
+++ ExercicioF/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioF
+{
+    class Program
+    {
+        static int BuscaSequencial(double [] b, int tl, double valor)
+        {
+            int i = 0;
+            int retorno = 0;
+            while(i<tl && b[i]!= valor)
+            {
+                i++;
+            }
+            if(i>=tl)
+            {
+                i = -1;
+            }
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            double[] a = new double[30];
+            double[] b = new double[30];
+            int i=0;
+            int tl = 30;
+            int pos = 0;
+            double valor; 
+
+            Console.WriteLine("Digite um matriz com 30 numeros:");
+            for (i = 0; i < tl; i++)
+            {
+                a[i] = double.Parse(Console.ReadLine());
+            }
+           for (i = 0; i < tl; i++)
+               {
+                    b[i]=a[i]*a[i]*a[i];
+               }
+            Console.WriteLine("Esse são os valores que temos para pesquisar:");
+            for (i = 0; i < tl; i++)
+                     {
+                        Console.Write(b[i]);
+                        Console.WriteLine();
+                     }
+            Console.WriteLine("Digite um valor que voce deseja pesquisar?");
+            Console.WriteLine(valor = double.Parse(Console.ReadLine()));
+            pos = BuscaSequencial(b,tl,valor);
+            if (pos == -1 )
+            {
+                Console.WriteLine("Valor não encontrado");
+            }
+            else
+            {
+                Console.WriteLine("Valor encontrado na posicao:"+pos);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExercicoG/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicoG
+{
+    class Program
+    {
+       static int pesquisaBinaria( int [] vetor,int tl, int chave)
+        {
+            int meio;
+            int min = 0;
+            int max = tl - 1;
+            int p = -1;
+            do
+            {
+                meio = (int)(min + max) / 2;
+                if (vetor[meio] == chave)
+                {
+                    //Retorna  a posição  do numero  na sequencia
+                    p= meio;
+                    break;
+                }
+                else if (chave > vetor[meio])
+                    min = meio + 1;
+                else
+                    max = meio - 1;
+            } while (min <= max);
+            return p;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = new int[20];
+            int[] b = new int[20];
+            int i = 0;
+            int j = 0;
+            int tl = 20;
+            int pos = 0;
+            int x;
+            int valor;
+            Console.WriteLine("Digite um matriz con 20 numeros:");
+            for (i = 0; i < tl; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                b[i] = a[i] + 2;
+            }
+            for (i = 0; i < tl; i++)
+            {
+                for (j = i + 1; j < tl; j++)
+                {
+                    if (b[i] > b[j])
+                    {
+                        x = b[i];
+                        b[i] = b[j];
+                        b[j] = x;
+                    }
+                }
+            }
+            Console.WriteLine("Esse são os valores que temos para pesquisar:");
+            for (i = 0; i < tl; i++)
+            {
+                Console.Write(b[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Digite um valor que voce deseja pesquisar?");
+            Console.WriteLine(valor = int.Parse(Console.ReadLine()));
+            pos = pesquisaBinaria(b, tl, valor);
+            if (pos == -1)
+            {
+                Console.WriteLine("Valor não encontrado");
+            }
+            else
+            {
+                Console.WriteLine("Valor encontrado na posicao:" + pos);
+            }
+            Console.ReadKey();
+        }
+    }
+}
